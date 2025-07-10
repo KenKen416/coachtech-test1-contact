48ed6c0b3fb57261121b0673e9f60aff800bc7d3
KenKen <kenkenstudy416@gmail.com>
2025-07-10 23:19:44 +0900

    完成

--- src/routes/web.php
+++ src/routes/web.php
@@ -32,6 +32,6 @@ Route::middleware('auth')->group(function(){
 });
 
 
-Route::get('/login', [LoginController::class, 'showLoginForm']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
